0bd69b83997a95cad55ba5a2df0d141fdfe7517b
wlidBirmou <laakab@gmail.com>
2023-07-27 10:57:59 -0400

    Add image for default recipe at startup

--- src/main/java/guru/springframework/bootstrap/DataLoader.java
+++ src/main/java/guru/springframework/bootstrap/DataLoader.java
@@ -6,27 +6,27 @@ import guru.springframework.domain.Notes;
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
 import java.util.LinkedHashSet;
 
 @Component
 @Slf4j
+@AllArgsConstructor
 public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
     private final RecipeRepository recipeRepository;
     private final UnitOfMeasureRepository unitOfMeasureRepository;
+    private final ResourceLoader resourceLoader;
 
-    @Autowired
-    public DataLoader(RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.recipeRepository = recipeRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
-    }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -37,6 +37,18 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
         guacamole.setNotes(Notes.builder().recipeNotes("recipeNotes").build());
         guacamole.setDifficulty(Difficulty.EASY);
         guacamole.setDirections("One cut avocado, remove flesh: cut the avocados in half. remove seed score the inside of the avocado");
+
+        try {
+            byte[] guacamoleImageUnboxed=Files.readAllBytes(this.resourceLoader.getResource("classpath:static/images/guacamole400x400.jpg").getFile().toPath());
+            Byte[] guacamoleImageBoxed=new Byte[guacamoleImageUnboxed.length];
+            int i=0;
+            for(byte b: guacamoleImageUnboxed){
+                guacamoleImageBoxed[i++]=Byte.valueOf(b);
+            }
+            guacamole.setImage(guacamoleImageBoxed);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         guacamole.getIngredients().add(new Ingredient("Ripe avocados", BigDecimal.valueOf(2),unitOfMeasureRepository.findByDescription("Unit").get(),guacamole));
         guacamole.getIngredients().add(new Ingredient("Kosher salt", BigDecimal.valueOf(0.5),unitOfMeasureRepository.findByDescription("Teaspoon").get(),guacamole));
         guacamole.getIngredients().add(new Ingredient("Lemon juice", BigDecimal.valueOf(0.25),unitOfMeasureRepository.findByDescription("Tbsp").get(),guacamole));
@@ -60,6 +72,19 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
                 "Any and every kind of leftover can go inside a warm tortilla, usually with a healthy dose of pickled jalapenos. I can always sniff out a late-night snacker when the aroma of tortillas heating in a hot pan on the stove comes wafting through the house.");
         grilledChicken.setNotes(grilledChickenNotes);
         grilledChicken.setDifficulty(Difficulty.MODERATE);
+
+        try {
+            byte[] grilledChickenImageUnboxed=Files.readAllBytes(this.resourceLoader.getResource("classpath:static/images/tacos400x400.jpg").getFile().toPath());
+            Byte[] grilledChickenImageBoxed=new Byte[grilledChickenImageUnboxed.length];
+            int j=0;
+            for(byte b: grilledChickenImageUnboxed){
+                grilledChickenImageBoxed[j++]=Byte.valueOf(b);
+            }
+            grilledChicken.setImage(grilledChickenImageBoxed);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         grilledChicken.setDescription("Spicy grilled chicken tacos");
         grilledChicken.getIngredients().add(new Ingredient("Ancho chili powder", BigDecimal.valueOf(2),unitOfMeasureRepository.findByDescription("Tablespoon").get(),grilledChicken));
         grilledChicken.getIngredients().add(new Ingredient("Dried oregano", BigDecimal.valueOf(1),unitOfMeasureRepository.findByDescription("Teaspoon").get(),grilledChicken));
