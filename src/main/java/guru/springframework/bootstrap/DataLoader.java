023850bbe2e9f53d961281cce77aa0442941e88e
wlidBirmou <laakab@gmail.com>
2023-07-30 23:43:57 -0400

    Creation of dev, prod, default datasources

--- src/main/java/guru/springframework/bootstrap/DataLoader.java
+++ src/main/java/guru/springframework/bootstrap/DataLoader.java
@@ -9,6 +9,7 @@ import guru.springframework.repositories.UnitOfMeasureRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -21,6 +22,7 @@ import java.util.LinkedHashSet;
 @Component
 @Slf4j
 @AllArgsConstructor
+@Profile("default")
 public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
     private final RecipeRepository recipeRepository;
